20ec2fdd05550443c02ff57b05557d4672234804
Wasiu200 <wasiuasimiyu5@gmail.com>
2025-10-28 04:39:25 -1200

    portfolio init

--- src/Portfolio/Contact.jsx
+++ src/Portfolio/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
         <div className="map-container">
           <iframe
             title="map"
-            src=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.2654096684755!2d4.598641674072423!3d7.76165220754008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103827489a44ee03%3A0xc8d59bcaa8bfd123!2sOsun%20State%20University!5e0!3m2!1sen!2sng!4v1761662737742!5m2!1sen!2sng" 
             width="100%"
             height="300"
             style={{ border: 0 }}
